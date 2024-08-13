dc50a803ecba471d1bebccbd477471c004385ac6
samadhan <samadhan.mhaske@frnd.app>
2024-08-13 15:51:39 +0530

    texting

--- preapp/urls.py
+++ preapp/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 #  jdjd
 # jhd
+print(f"hello")
